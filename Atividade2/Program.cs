a84c248c2c11e3a6317608233429d9978131a155
Leonardo Belinato <70040882+haid3@users.noreply.github.com>
2022-05-24 00:42:01 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.WriteLine("Informe seu Primeiro Nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Informe sua Idade");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe seu Telefone");
+            int telefone = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o salário");
+            float salario = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nome Informado foi: " + nome);
+            Console.WriteLine("Idade Informada foi: " + idade);
+            Console.WriteLine("Telefone Informado foi: " + telefone);
+            Console.WriteLine("O salário informado foi: " + salario);
+            Console.Read();
+        }
+    }
+}
